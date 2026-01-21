6e5b99067449f4feeaf2b2bd5eb78d25b5873dfb
justyn-clark <ijustynclark@gmail.com>
2026-01-20 20:08:09 -0800

    Add SMALL formula and auto-update workflow
    - Add Formula/small.rb for SMALL CLI installation
    - Add workflow to automatically update formula on new releases
    - Add README with installation instructions

---
+++ Formula/small.rb
@@ -0,0 +1,41 @@
+# typed: false
+# frozen_string_literal: true
+
+class Small < Formula
+  desc "SMALL CLI - Command line interface for the SMALL protocol"
+  homepage "https://github.com/justyn-clark/small-dist"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    on_intel do
+      url "https://github.com/justyn-clark/small-dist/releases/download/v#{version}/small_darwin_amd64.tar.gz"
+      sha256 "PLACEHOLDER_DARWIN_AMD64_SHA256"
+    end
+
+    on_arm do
+      url "https://github.com/justyn-clark/small-dist/releases/download/v#{version}/small_darwin_arm64.tar.gz"
+      sha256 "PLACEHOLDER_DARWIN_ARM64_SHA256"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/justyn-clark/small-dist/releases/download/v#{version}/small_linux_amd64.tar.gz"
+      sha256 "PLACEHOLDER_LINUX_AMD64_SHA256"
+    end
+
+    on_arm do
+      url "https://github.com/justyn-clark/small-dist/releases/download/v#{version}/small_linux_arm64.tar.gz"
+      sha256 "PLACEHOLDER_LINUX_ARM64_SHA256"
+    end
+  end
+
+  def install
+    bin.install "small"
+  end
+
+  test do
+    system "#{bin}/small", "version"
+  end
+end
---
+++ README.md
@@ -0,0 +1,29 @@
+# Homebrew Tap for SMALL CLI
+
+This is the official Homebrew tap for the SMALL CLI.
+
+## Installation
+
+```sh
+brew tap justyn-clark/tap
+brew install small
+```
+
+## Upgrade
+
+```sh
+brew upgrade small
+```
+
+## Uninstall
+
+```sh
+brew uninstall small
+brew untap justyn-clark/tap
+```
+
+## Available Formulae
+
+| Formula | Description |
+|---------|-------------|
+| small   | SMALL CLI - Command line interface for the SMALL protocol |
