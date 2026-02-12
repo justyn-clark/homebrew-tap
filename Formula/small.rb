2f437a84d50cf4174cc16f550891b3b9cd4bd679
Justin Johnson <ijustynclark@gmail.com>
2026-02-11 16:37:52 -0800

    chore(release): small v1.0.1

--- Formula/small.rb
+++ Formula/small.rb
@@ -4,30 +4,30 @@
 class Small < Formula
   desc "Command-line interface for the SMALL protocol"
   homepage "https://github.com/justyn-clark/small-protocol"
-  version "1.0.0"
+  version "1.0.1"
   license "Apache-2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/justyn-clark/small-protocol/releases/download/v#{version}/small-protocol_1.0.0_Darwin_x86_64.tar.gz"
-      sha256 "b027e0122f9a4b9af7af4ab481055e72ebd1e573902447abcd641ac244f2771f"
+      url "https://github.com/justyn-clark/small-protocol/releases/download/v#{version}/small-protocol_1.0.1_Darwin_x86_64.tar.gz"
+      sha256 "655c00bb9bbf6001896450cd2b2165d68da8fd98837e4cca1b49782982a919c7"
     end
 
     on_arm do
-      url "https://github.com/justyn-clark/small-protocol/releases/download/v#{version}/small-protocol_1.0.0_Darwin_arm64.tar.gz"
-      sha256 "e10a82869773e24f0d4f95ff409150678f7d8c970126cb69c560ab1117d325d7"
+      url "https://github.com/justyn-clark/small-protocol/releases/download/v#{version}/small-protocol_1.0.1_Darwin_arm64.tar.gz"
+      sha256 "78b46429290979bcc1dfd92e8c4acbbe6744f46f34f30b72c55079f5d3931a15"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/justyn-clark/small-protocol/releases/download/v#{version}/small-protocol_1.0.0_Linux_x86_64.tar.gz"
-      sha256 "537600a09b45bb107198af45a0d2ac453f2cf11cf6aabf6030464576a7fdc2f2"
+      url "https://github.com/justyn-clark/small-protocol/releases/download/v#{version}/small-protocol_1.0.1_Linux_x86_64.tar.gz"
+      sha256 "668920f37e7e220d045f0c09b33f411e104c1a4e70b33ac7570abb222e9bd62a"
     end
 
     on_arm do
-      url "https://github.com/justyn-clark/small-protocol/releases/download/v#{version}/small-protocol_1.0.0_Linux_arm64.tar.gz"
-      sha256 "f736f56a00638d8c22dccd3f7ac3d750444c3498a7f2434924a445293663eb46"
+      url "https://github.com/justyn-clark/small-protocol/releases/download/v#{version}/small-protocol_1.0.1_Linux_arm64.tar.gz"
+      sha256 "721d2bb9122addc2a83fac867b537f0f9ddd7430b07ebd4fd1d4bec52753e2fe"
     end
   end
 
